6285635ec14e95b41ffeac8cac22ec9c8d128335
Christo Marais <38477955@mynwu.ac.za>
2024-04-30 21:37:44 +0200

    Merge branch 'development' of https://github.com/r-swrt-GB/cmpg-315-group-5 into development

---
+++ Text-Message-CMPG-315-Poject/Text-Message-CMPG-315-Poject/Forms/Backend_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Message_CMPG_315_Poject.Forms
+{
+    public partial class Backend_Test : Form
+    {
+        public Backend_Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
