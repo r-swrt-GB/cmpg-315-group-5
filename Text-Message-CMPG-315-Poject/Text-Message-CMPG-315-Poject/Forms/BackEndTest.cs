5129ff1409a94c0f2bf92ccb7ae79b2d6a463d4f
Gerhard Otto <gerrieotto123@gmail.com>
2024-05-01 13:28:02 +0200

    Add comments

--- Text-Message-CMPG-315-Poject/Text-Message-CMPG-315-Poject/Forms/BackEndTest.cs
+++ Text-Message-CMPG-315-Poject/Text-Message-CMPG-315-Poject/Forms/BackEndTest.cs
@@ -11,6 +11,10 @@ using Google.Cloud.Firestore;
 using Text_Message_CMPG_315_Poject.Classes;
 using Text_Message_CMPG_315_Poject.Forms;
 
+
+//Info
+//Async maak dat die program kan aanhou run al is die function nog nie klaar nie
+//Task is 'n async function
 namespace Text_Message_CMPG_315_Poject.Forms
 {
     public partial class BackEndTest : Form
@@ -22,10 +26,13 @@ namespace Text_Message_CMPG_315_Poject.Forms
 
         public async Task<string> FindUserIdByEmail(FirestoreDb database, string email)
         {
+            //Soek deur die collections vir die email wie die message moet kry
             CollectionReference usersCollection = database.Collection("users");
             Query query = usersCollection.WhereEqualTo("Email", email);
+            //Return al die collections wat == aan die email
             QuerySnapshot snapshot = await query.GetSnapshotAsync();
 
+            //Ensure only one email
             if (snapshot.Count == 1)
             {
                 DocumentSnapshot userDocument = snapshot.Documents[0];
@@ -36,6 +43,7 @@ namespace Text_Message_CMPG_315_Poject.Forms
 
         public async Task SendMessageAsync(FirestoreDb database, string senderEmail, string recipientEmail, string messageBody)
         {
+            //Ensure email receiver and sender exists
             string senderId = await FindUserIdByEmail(database, senderEmail);
             string recipientId = await FindUserIdByEmail(database, recipientEmail);
 
@@ -45,15 +53,17 @@ namespace Text_Message_CMPG_315_Poject.Forms
                 return;
             }
 
+            //Create message
             Messages newMessage = new Messages()
             {
                 body = messageBody,
-                created_at = DateTime.UtcNow,
+                created_at = DateTime.UtcNow,//UtcNwo used for FireStore requirement
                 recipient_id = recipientId,
                 sender_id = senderId,
                 read_at = null  //message hasn't been read 
             };
 
+            //Get firestore unique ID
             CollectionReference messagesCollection = database.Collection("messages");
             DocumentReference newDocRef = await messagesCollection.AddAsync(newMessage);
             MessageBox.Show("Message sent successfully. ID: " + newDocRef.Id);
