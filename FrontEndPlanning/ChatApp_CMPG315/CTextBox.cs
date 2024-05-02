6b10b2fbec12c0c3b116ed0c77413b70e849be21
Reuben Thorn <reuben.j.thorn@icloud.com>
2024-05-02 05:55:22 +0200

    Added begining of front end files

---
+++ FrontEndPlanning/ChatApp_CMPG315/CTextBox.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatApp_CMPG315
+{
+
+    [DefaultEvent ("_TextChanged")]
+    public partial class CTextBox : UserControl
+    {
+
+        //fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        private Color borderFocusColor = Color.HotPink;
+        private bool isFocused = false;
+
+        public CTextBox()
+        {
+            InitializeComponent();
+
+        }
+
+
+        //events
+        public event EventHandler _TextChanged;
+
+        //properties
+        [Category("TextBox Custom properties")]
+        public Color BorderColor { get { return borderColor; } set { borderColor = value; this.Invalidate(); } }
+       
+        [Category("TextBox Custom properties")]
+        public int BorderSize { get { return borderSize; } set { borderSize = value; this.Invalidate(); } }
+
+        [Category("TextBox Custom properties")]
+        public bool UnderlinedStyle { get { return underlinedStyle; } set { underlinedStyle = value; this.Invalidate(); } }
+        //this.Invalidate() redraw die control
+
+        [Category("TextBox Custom properties")]
+        public bool PasswordChar
+        {
+            get { return textBox1.UseSystemPasswordChar; }
+            set { textBox1.UseSystemPasswordChar = value; }
+        }
+
+        public bool Multiline
+        {
+            get { return textBox1.Multiline; }
+            set { textBox1.Multiline = value; }
+        }
+
+        [Category("TextBox Custom properties")]
+        public override Color BackColor { get => base.BackColor; set { base.BackColor = value; textBox1.BackColor = value; } }
+
+        [Category("TextBox Custom properties")]
+        public override Color ForeColor { get => base.ForeColor; set { base.ForeColor = value; textBox1.ForeColor = value; } }
+
+        [Category("TextBox Custom properties")]
+        public override Font Font { get => base.Font;
+            set
+            {
+                base.Font = value; textBox1.Font = value;
+                if (this.DesignMode)
+                    UpdateControlHeight();
+            }
+        }
+
+        [Category("TextBox Custom properties")]
+        public string Texts { get { return textBox1.Text;  } set { textBox1.Text = value; } }
+
+        [Category("TextBox Custom properties")] 
+        public Color BorderFocusColor { get => borderFocusColor; set => borderFocusColor = value; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Draw border
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+                if (!isFocused)
+                {
+                    if (underlinedStyle)
+                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    else //normal style
+                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                }
+                else
+                {
+                    penBorder.Color = borderFocusColor;
+
+                    if (underlinedStyle)
+                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    else //normal style
+                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                }
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            UpdateControlHeight();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+        //private methods
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (_TextChanged != null)
+                _TextChanged.Invoke(sender, e);
+        }
+
+
+        //as nog enige event handelers in gesit moet word, gaan na CTextBox.cs [design] , dan
+        //double click op die event handeler daar, en insert this.On___(e);
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.OnClick(e);
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.OnMouseEnter(e);
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            this.OnMouseHover(e);
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.OnMouseLeave(e);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.OnKeyPress(e);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            isFocused = true;
+            this.Invalidate();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            isFocused = false;
+            this.Invalidate();
+        }
+        //+++
+    }
+
+
+
+}
---
+++ FrontEndPlanning/ChatApp_CMPG315/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatApp_CMPG315
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cTextBox3__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
