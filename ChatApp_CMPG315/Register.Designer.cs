5b75bb81b6a10b052aee031501724d04168753a4
caityD <142865980+caityD@users.noreply.github.com>
2024-05-06 19:58:29 +0200

    Caitlyn - Forms
    Completed the Login and registration forms

--- ChatApp_CMPG315/Register.Designer.cs
+++ ChatApp_CMPG315/Register.Designer.cs
@@ -29,12 +29,294 @@ namespace ChatApp_CMPG315
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.cRegAcc = new ChatApp_CMPG315.CButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cTxtName = new ChatApp_CMPG315.CTextBox();
+            this.cTxtSName = new ChatApp_CMPG315.CTextBox();
+            this.cTxtEmail = new ChatApp_CMPG315.CTextBox();
+            this.cTxtPassword = new ChatApp_CMPG315.CTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(187, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "REGISTER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(163, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NAME:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel1.Location = new System.Drawing.Point(155, 303);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 1);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel2.Location = new System.Drawing.Point(155, 354);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(280, 1);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel3.Location = new System.Drawing.Point(155, 415);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(280, 1);
+            this.panel3.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel4.Location = new System.Drawing.Point(155, 475);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(280, 1);
+            this.panel4.TabIndex = 7;
+            // 
+            // cRegAcc
+            // 
+            this.cRegAcc.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.cRegAcc.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.cRegAcc.BorderColor = System.Drawing.Color.PaleTurquoise;
+            this.cRegAcc.BorderRadius = 30;
+            this.cRegAcc.BorderSize = 0;
+            this.cRegAcc.FlatAppearance.BorderSize = 0;
+            this.cRegAcc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cRegAcc.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cRegAcc.ForeColor = System.Drawing.Color.White;
+            this.cRegAcc.Location = new System.Drawing.Point(195, 513);
+            this.cRegAcc.Name = "cRegAcc";
+            this.cRegAcc.Size = new System.Drawing.Size(191, 40);
+            this.cRegAcc.TabIndex = 8;
+            this.cRegAcc.Text = "REGISTER ACCOUNT";
+            this.cRegAcc.TextColor = System.Drawing.Color.White;
+            this.cRegAcc.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(163, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "SURNAME:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(163, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "EMAIL:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(163, 454);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "PASSWORD:";
+            // 
+            // cTxtName
+            // 
+            this.cTxtName.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.cTxtName.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.cTxtName.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.cTxtName.BorderSize = 2;
+            this.cTxtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cTxtName.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.cTxtName.Location = new System.Drawing.Point(209, 267);
+            this.cTxtName.Margin = new System.Windows.Forms.Padding(4);
+            this.cTxtName.Multiline = false;
+            this.cTxtName.Name = "cTxtName";
+            this.cTxtName.Padding = new System.Windows.Forms.Padding(7);
+            this.cTxtName.PasswordChar = false;
+            this.cTxtName.Size = new System.Drawing.Size(226, 31);
+            this.cTxtName.TabIndex = 12;
+            this.cTxtName.Texts = "";
+            this.cTxtName.UnderlinedStyle = false;
+            // 
+            // cTxtSName
+            // 
+            this.cTxtSName.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.cTxtSName.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.cTxtSName.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.cTxtSName.BorderSize = 2;
+            this.cTxtSName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cTxtSName.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.cTxtSName.Location = new System.Drawing.Point(229, 320);
+            this.cTxtSName.Margin = new System.Windows.Forms.Padding(4);
+            this.cTxtSName.Multiline = false;
+            this.cTxtSName.Name = "cTxtSName";
+            this.cTxtSName.Padding = new System.Windows.Forms.Padding(7);
+            this.cTxtSName.PasswordChar = false;
+            this.cTxtSName.Size = new System.Drawing.Size(206, 31);
+            this.cTxtSName.TabIndex = 13;
+            this.cTxtSName.Texts = "";
+            this.cTxtSName.UnderlinedStyle = false;
+            // 
+            // cTxtEmail
+            // 
+            this.cTxtEmail.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.cTxtEmail.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.cTxtEmail.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.cTxtEmail.BorderSize = 2;
+            this.cTxtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cTxtEmail.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.cTxtEmail.Location = new System.Drawing.Point(209, 381);
+            this.cTxtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.cTxtEmail.Multiline = false;
+            this.cTxtEmail.Name = "cTxtEmail";
+            this.cTxtEmail.Padding = new System.Windows.Forms.Padding(7);
+            this.cTxtEmail.PasswordChar = false;
+            this.cTxtEmail.Size = new System.Drawing.Size(226, 31);
+            this.cTxtEmail.TabIndex = 14;
+            this.cTxtEmail.Texts = "";
+            this.cTxtEmail.UnderlinedStyle = false;
+            // 
+            // cTxtPassword
+            // 
+            this.cTxtPassword.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.cTxtPassword.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.cTxtPassword.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.cTxtPassword.BorderSize = 2;
+            this.cTxtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cTxtPassword.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.cTxtPassword.Location = new System.Drawing.Point(240, 441);
+            this.cTxtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.cTxtPassword.Multiline = false;
+            this.cTxtPassword.Name = "cTxtPassword";
+            this.cTxtPassword.Padding = new System.Windows.Forms.Padding(7);
+            this.cTxtPassword.PasswordChar = false;
+            this.cTxtPassword.Size = new System.Drawing.Size(195, 31);
+            this.cTxtPassword.TabIndex = 15;
+            this.cTxtPassword.Texts = "";
+            this.cTxtPassword.UnderlinedStyle = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ChatApp_CMPG315.Properties.Resources.Speech;
+            this.pictureBox2.Location = new System.Drawing.Point(209, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 93);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChatApp_CMPG315.Properties.Resources.icons8_close_window_48;
+            this.pictureBox1.Location = new System.Drawing.Point(552, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 15);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ChatApp_CMPG315.Properties.Resources.icons8_eye_60;
+            this.pictureBox3.Location = new System.Drawing.Point(442, 450);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(22, 22);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(580, 583);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.cTxtPassword);
+            this.Controls.Add(this.cTxtEmail);
+            this.Controls.Add(this.cTxtSName);
+            this.Controls.Add(this.cTxtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cRegAcc);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Register";
             this.Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private CButton cRegAcc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private CTextBox cTxtName;
+        private CTextBox cTxtSName;
+        private CTextBox cTxtEmail;
+        private CTextBox cTxtPassword;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
